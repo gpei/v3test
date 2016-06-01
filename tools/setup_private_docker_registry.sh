c9f40b6421a4d8287db02a91517e48f3977ef390
gpei <gpei@redhat.com>
2016-06-01 10:56:48 +0800

    tt

--- tools/setup_private_docker_registry.sh
+++ tools/setup_private_docker_registry.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 #conf_hostname=
-#conf_GA_image_source=
-#conf_new_image_source=
-#conf_xpass_registry_server=
-conf_GA_version="v3.0.0.1"
-ose_infra_images="openshift3/ose-docker-registry
-openshift3/ose-haproxy-router
-openshift3/ose-deployer
-openshift3/ose-sti-builder
-openshift3/ose-docker-builder
-openshift3/ose-keepalived-ipfailover
-openshift3/ose-pod"
+images="xxx"
 
 
 function clean_exist_image(){
@@ -32,17 +22,17 @@ function set_docker_host(){
 
 }
 
-function pull_ose_infra_images() {
-    for i in $ose_infra_images; do
-        docker pull "$GA_image_source/$i"
+function pull_images() {
+    for i in $images; do
+        docker pull "$image_source/$i"
     done
 }
 
 
 
-function push_GA_images(){
-    for i in $ose_infra_images; do
-        docker tag $GA_image_source/$i $registry_server/$i && docker push $registry_server/$i
+function push_images(){
+    for i in $images; do
+        docker tag $image_source/$i $registry_server/$i && docker push $registry_server/$i
     done
 }
 
@@ -52,8 +42,8 @@ yum install docker-registry -y
 hostname="${conf_hostname:-upgrade.registry.com}"
 set_docker_host
 
-GA_image_source="${conf_GA_image_source:-www.example.com}"
-pull_ose_infra_images
+image_source="${conf_image_source:-www.example.com}"
+pull_images
 
 registry_server=$hostname:5000
-push_GA_images
+push_images
